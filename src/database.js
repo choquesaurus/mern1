e9f529a1f8f81b07666da9e432236f57c3d8baa5
Wasauskyok <daniechoque159@gmail.com>
2020-01-11 06:42:22 -0500

    add  .env

--- src/database.js
+++ src/database.js
@@ -1,5 +1,4 @@
+require('dotenv').config();
 const  mongoose=require('mongoose');
-
-const url='mongodb+srv://gouser2:1234@cluster0-bzauq.mongodb.net/mitienda?retryWrites=true&w=majority&authSource=admin';
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:true}).then(db=>console.log(`db is connected ${db}`)).catch(erro=>console.log(erro));
+mongoose.connect(process.env.url,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:true}).then(db=>console.log(`db is connected ${db}`)).catch(erro=>console.log(erro));
 module.exports=mongoose;
